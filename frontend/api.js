1284872bfdb7db5a2adfe67f14a5fa969491af22
Sriniketh <srinikethvangipuram996@gmail.com>
2025-11-04 02:34:56 +0530

    Api url

--- frontend/api.js
+++ frontend/api.js
@@ -1,2 +1,7 @@
-// frontend/api.js
-export const API_BASE_URL = "https://loan-backend-3ml6.onrender.com/api";
+// config.js
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://your-app-name.onrender.com/api"
+    : "http://localhost:5000/api";
+
+export default API_URL;
